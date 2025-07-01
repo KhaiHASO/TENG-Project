ea8339e9187d3d0a49a22b8e21eec8d3692a80e5
sesgrinder <levhg2267@gmail.com>
2025-06-30 20:47:20 -0400

    Integrate physical interface
    Integrate physical interface, including get_sensor_health() and generate_sensor_data(). Each of those functions' return is self-explainatory in be/api/marine_sensor/sensor_parser.py
    The backend will now start with openning port 5000 and open serial connection to RX.
    
    Add a requirement for `serial` package

--- be/start.py
+++ be/start.py
@@ -1,6 +1,12 @@
 import os
 from app import app
-
+import threading
+from api.marine_sensor import sensor_parser
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
+    sensor_parser.init_sensor_parser()
+
+    sensor_thread = threading.Thread(sensor_parser.sensor_read)
+    sensor_thread.start()
+    
     app.run(host='0.0.0.0', port=port, debug=True) 
